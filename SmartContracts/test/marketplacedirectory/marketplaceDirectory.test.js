31ebafe1bc528590ee4777061651c698eabffb65
unknown <wesley@vanheije.net>
2018-03-12 18:11:16 +0000

    added marketplace tests

--- SmartContracts/test/marketplacedirectory/marketplaceDirectory.test.js
+++ SmartContracts/test/marketplacedirectory/marketplaceDirectory.test.js
@@ -7,12 +7,17 @@ contract('MarketplaceDirectory', function(accounts) {
     const defaultConsortiumHash = web3.sha3(defaultConsortiumName);
     const defaultAccount = accounts[0];
 
-    const participant1 = accounts[1];
-    const participant2 = accounts[2];
-    const participant3 = accounts[3];
+    const companyA = "Company A";
+    const companyAWallet = accounts[1];
+    const companyB = "Company B";
+    const companyBWallet = accounts[2];
+    const memberA1 = "Member A1";
+    const memberA1Wallet = accounts[3];
+    const memberA2 = "Member A2";
+    const memberA2Wallet = accounts[4];
 
-    const wallet1 = accounts[4];
-    const wallet2 = accounts[5];
+    const wallet1 = accounts[5];
+    const wallet2 = accounts[6];
 
     const startOfYear = 1514808000;
     const midpointOfYear = 1530532800;
@@ -61,8 +66,7 @@ contract('MarketplaceDirectory', function(accounts) {
 
         it ('should be possible to add a new participant', async() => { 
 
-            var name = "ForceField participant 1";
-            var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, name, participant1);
+            var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, companyA, companyAWallet);
 
             assert.isNotNull(result);
             assert.isNotNull(result.tx);
@@ -70,11 +74,9 @@ contract('MarketplaceDirectory', function(accounts) {
 
         it ('should NOT be possible to add a new particpant from unknown parent', async() => { 
 
-            var name = "ForceField participant 1";
-
             // TODO: Expected exception as helper function
             try {
-                var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, name, participant1, { from: participant1 });
+                var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, companyA, companyAWallet, { from: companyAWallet });
             }
             catch (error) {
                 const invalidOpcode = error.message.search('invalid opcode') >= 0;
@@ -90,53 +92,86 @@ contract('MarketplaceDirectory', function(accounts) {
 
         it ('should be possible to get the new participant', async() => { 
 
-            var name = "ForceField participant 1";
-            var participantDetails = await MarketplaceDirectory.participant(name);
+            var participantDetails = await MarketplaceDirectory.participant(companyA);
             
             assert.strictEqual(5, participantDetails.length);
             assert.strictEqual(defaultConsortiumHash, participantDetails[0]);
             assert.strictEqual(startOfYear, participantDetails[1].toNumber());
             assert.strictEqual(endOfYear, participantDetails[2].toNumber());
-            assert.strictEqual(name, participantDetails[3]);
-            assert.strictEqual(participant1, participantDetails[4]);
+            assert.strictEqual(companyA, participantDetails[3]);
+            assert.strictEqual(companyAWallet, participantDetails[4]);
         })
 
         it ('should be possible to update an existing participant', async() => { 
 
-            var name = "ForceField participant 1";
-            var result = await MarketplaceDirectory.updateParticipant(midpointOfYear, endOfYear, name, participant1);
+            var result = await MarketplaceDirectory.updateParticipant(midpointOfYear, endOfYear, companyA, companyAWallet);
             
             assert.isNotNull(result);
             assert.isNotNull(result.tx);
 
-            var participantDetails = await MarketplaceDirectory.participant(name);
+            var participantDetails = await MarketplaceDirectory.participant(companyA);
             
             assert.strictEqual(5, participantDetails.length);
             assert.strictEqual(defaultConsortiumHash, participantDetails[0]);
             assert.strictEqual(midpointOfYear, participantDetails[1].toNumber());
             assert.strictEqual(endOfYear, participantDetails[2].toNumber());
-            assert.strictEqual(name, participantDetails[3]);
-            assert.strictEqual(participant1, participantDetails[4]);
+            assert.strictEqual(companyA, participantDetails[3]);
+            assert.strictEqual(companyAWallet, participantDetails[4]);
         })
 
         it ('should be possible to update an existing participant with a new wallet address', async() => { 
 
-            var name = "ForceField participant 1";
-            var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, name, wallet1);
+            var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, companyA, wallet1);
             
             assert.isNotNull(result);
             assert.isNotNull(result.tx);
 
-            var participantDetails = await MarketplaceDirectory.participant(name);
+            var participantDetails = await MarketplaceDirectory.participant(companyA);
             
             assert.strictEqual(5, participantDetails.length);
             assert.strictEqual(defaultConsortiumHash, participantDetails[0]);
             assert.strictEqual(startOfYear, participantDetails[1].toNumber());
             assert.strictEqual(endOfYear, participantDetails[2].toNumber());
-            assert.strictEqual(name, participantDetails[3]);
+            assert.strictEqual(companyA, participantDetails[3]);
             assert.strictEqual(wallet1, participantDetails[4]);
         })
 
-        // should be possible to add a (sub)participant
+        it ('should be possible to add a new participant from an added parent', async() => { 
+
+            var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, companyB, companyBWallet, { from: companyAWallet });
+            
+            assert.isNotNull(result);
+            assert.isNotNull(result.tx);
+
+            var participantDetails = await MarketplaceDirectory.participant(companyB);
+
+            // console.log(participantDetails);
+            // var hashedB = web3.sha3(companyB);
+            // var hashedA = web3.sha3(companyA);
+
+            // console.log("Company A: " + companyA);
+            // console.log("Company A Wallet: " + companyAWallet);
+            // console.log("Company A Hashed: " + hashedA);
+
+            // console.log("Company B: " + companyB);
+            // console.log("Company B Wallet: " + companyBWallet);
+            // console.log("Company B Hashed: " + hashedB);
+
+            assert.strictEqual(5, participantDetails.length);
+            assert.strictEqual(web3.sha3(companyA), participantDetails[0]);
+            assert.strictEqual(startOfYear, participantDetails[1].toNumber());
+            assert.strictEqual(endOfYear, participantDetails[2].toNumber());
+            assert.strictEqual(companyB, participantDetails[3]);
+            assert.strictEqual(companyBWallet, participantDetails[4]);
+        })
+        
+
+        // it ('should be possible to add a same participant under a different parent', async() => { 
+
+        //     var result = await MarketplaceDirectory.updateParticipant(startOfYear, endOfYear, companyB, companyBWallet);
+
+        //     assert.isNotNull(result);
+        //     assert.isNotNull(result.tx);
+        // })
     })
 })
