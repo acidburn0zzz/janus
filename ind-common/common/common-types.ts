0c2aca6b3f2fbb3a67ccd0b622aa60596aab06e6
Aravind <aravindgrao@gmail.com>
2018-04-08 12:05:33 -0500

    Added interfaces for PostTransaction

--- ind-common/common/common-types.ts
+++ ind-common/common/common-types.ts
@@ -3,8 +3,12 @@ export type KeyFields = { key: string; fields: string[] };
 export interface DecryptDataRequestMessage extends BaseMessageObject { keys: KeyFields[]; data: {} };
 export interface EncryptDataRequestMessage extends BaseMessageObject { keys: KeyFields[]; data: {} };
 export interface GrantAccessRequestMessage extends BaseMessageObject {
-    accessibleSymmetricKey: string,
-    partyOTAddress: string,
-    partyBitcorePublicKey: string,
-    contractAddress: string,
+    accessibleSymmetricKey: string;
+    partyOTAddress: string;
+    partyBitcorePublicKey: string;
+    contractAddress: string;
 };
+
+export interface PostTxnOtherInfo {
+    
+}
